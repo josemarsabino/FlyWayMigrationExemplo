cfef633e40ef09a694cef14976e81b4a9183c0df
Josemar Sabino <josemarsabino@hotmail.com>
2021-07-09 09:01:09 -0300

    Projeto Inicial

---
+++ src/main/java/br/com/sada/MigrationFlyWayApplication.java
@@ -0,0 +1,13 @@
+package br.com.sada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrationFlyWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigrationFlyWayApplication.class, args);
+	}
+
+}
